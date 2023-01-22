5828020b3a4307fbe566b0a702baf99b2bad0c5b
240p60 <mr.alhoev@mail.ru>
2023-01-22 17:14:42 +0300

    update contract & setup contract event listener

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
+const CONTRACT_ADDRESS = "0xb3B2EFEc19CEA413e0775850B256E5aDaB7f1fc9";
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
@@ -41,8 +42,13 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener();
     } else {
       console.log("No authorized account found");
+      setCurrentAccount("");
     }
   }
 
@@ -68,13 +74,46 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
     } catch (error) {
       console.log(error);
     }
   }
 
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x2585D6963E7FE3741842067CeC52d3b8497F6dee";
   
     try {
       const { ethereum } = window;
@@ -100,49 +139,45 @@ const App = () => {
     }
   }
 
-  // Render Methods
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
-  /*
-  * This runs our function when the page loads.
-  */
-   useEffect(() => {
-    checkIfWalletIsConnected();
-  }, [])
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  )
+
+  console.log(currentAccount);
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
-          <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
-          </p>
-          {currentAccount === "" 
-            ? renderNotConnectedContainer()
-            : (
-              /** Add askContractToMintNft Action for the onClick event **/
-              <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-                Mint NFT
-              </button>
-            )
-          }
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
+    <div className="container">
+      <div className="header-container">
+        <p className="header gradient-text">My NFT Collection</p>
+        <p className="sub-text">
+          Each unique. Each beautiful. Discover your NFT today.
+        </p>
+        {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+      </div>
+      <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+          className="footer-text"
+          href={TWITTER_LINK}
+          target="_blank"
+          rel="noreferrer"
+        >{`built on @${TWITTER_HANDLE}`}</a>
       </div>
     </div>
+  </div>
   );
 };
 
